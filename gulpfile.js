6d0b8a2be158e8daac226d30082033551732f7bf
mortanbor <42118390+mortanbor@users.noreply.github.com>
2019-05-19 02:19:46 +0200

    Подготовка проекта

--- gulpfile.js
+++ gulpfile.js
@@ -13,80 +13,111 @@ var rename = require("gulp-rename");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var csso = require("gulp-csso");
+var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
 var server = require("browser-sync").create();
 var del = require("del");
 
 gulp.task("css", function () {
-return gulp.src("source/less/style.less")
-.pipe(plumber())
-.pipe(sourcemap.init())
-.pipe(less())
-.pipe(postcss([ autoprefixer() ]))
-.pipe(csso())
-.pipe(rename("style.min.css"))
-.pipe(sourcemap.write("."))
-.pipe(gulp.dest("build/css"))
-.pipe(server.stream());
+  return gulp.src("source/less/style.less")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(less())
+    .pipe(postcss([ autoprefixer() ]))
+    .pipe(gulp.dest("build/css"))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"));
 });
+
+gulp.task("js", function () {
+  return gulp.src([
+    "node_modules/picturefill/dist/picturefill.min.js",
+    "node_modules/svg4everybody/dist/svg4everybody.min.js",
+    "source/js/map.js",
+    "source/js/menu.js"
+  ])
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(concat('script.js'))
+    .pipe(gulp.dest("build/js"))
+    .pipe(uglify())
+    .pipe(rename("script.min.js"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/js"));
+});
+
 gulp.task("images", function () {
-return gulp.src("source/img/**/*.{png,jpg,svg}")
-.pipe(imagemin([
-imagemin.optipng({optimizationLevel: 3}),
-imagemin.jpegtran({progressive: true}),
-imagemin.svgo()
-]))
-.pipe(gulp.dest("source/img"));
+  return gulp.src("source/img/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("source/img"));
 });
+
 gulp.task("webp", function () {
-return gulp.src("source/img/**/*.{png,jpg}")
-.pipe(webp({quality: 90}))
-.pipe(gulp.dest("source/img"));
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("build/img"));
 });
+
 gulp.task("sprite", function () {
-return gulp.src("source/img/icon-*.svg")
-.pipe(svgstore({
-inlineSvg: true
-}))
-.pipe(rename("sprite.svg"))
-.pipe(gulp.dest("build/img"));
+return gulp.src("source/img/symbols/*.svg")
+  .pipe(imagemin([
+    imagemin.svgo()
+  ]))
+  .pipe(svgstore({
+    inlineSvg: true
+  }))
+  .pipe(rename("sprite.svg"))
+  .pipe(gulp.dest("build/img"));
 });
+
 gulp.task("html", function () {
-return gulp.src("source/*.html")
-.pipe(posthtml([
-include()
-]))
-.pipe(gulp.dest("build"));
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("build"));
 });
+
 gulp.task("clean", function () {
-return del("build");
+  return del("build");
 });
+
 gulp.task("copy", function () {
-return gulp.src([
-"source/fonts/**/*.{woff,woff2}",
-"source/img/**",
-"source/js/**"
-], {
-base: "source"
-})
-.pipe(gulp.dest("build"));
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/*.{png,jpg,svg}"
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
 });
+
 gulp.task("server", function () {
-server.init({
-server: "build/",
-notify: false,
-open: true,
-cors: true,
-ui: false
+  server.init({
+    server: "build/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/less/**/*.less", gulp.series("css", "refresh"));
+  gulp.watch("source/js/**/*.js", gulp.series("js", "refresh"));
+  gulp.watch("source/img/symbols/*.svg", gulp.series("sprite", "refresh"));
+  gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
 
-gulp.watch("source/less/**/*.less", gulp.series("css"));
-gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
-gulp.watch("source/*.html", gulp.series("html", "refresh"));
-});
 gulp.task("refresh", function (done) {
-server.reload();
-done();
+  server.reload();
+  done();
 });
 
-gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
-gulp.task("start", gulp.series("build", "server"));
+gulp.task("dev", gulp.series(gulp.parallel("copy", "css", "js", "sprite"), "html"));
+gulp.task("build", gulp.series("clean", "images", "dev", "webp"));
+gulp.task("start", gulp.series("dev", "server"));
--- source/js/map.js
+++ source/js/map.js
@@ -1,20 +1,21 @@
 /*карта*/
-var map;
-function initMap() {
-  var mapOptions = {
-    zoom: 7,
-    center: new google.maps.LatLng(34.868633, -111.761734),
-    disableDefaultUI: true
-  }
+if (document.querySelector('#map')) {
+  var map;
+  function initMap() {
+    var mapOptions = {
+      zoom: 7,
+      center: new google.maps.LatLng(34.868633, -111.761734),
+      disableDefaultUI: true
+    }
     map = new google.maps.Map(document.getElementById('map'), mapOptions);
     var image = "img/icon-map-marker.svg";
-    var myLatLng = new google.maps.LatLng(34.868633, -111.761734);
+    var myLatLng = new google.maps.LatLng(34.869943, -111.71734);
     var beachMarker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    icon: image
-  });
-}
+      position: myLatLng,
+      map: map,
+      icon: image
+    });
+  }
 
-google.maps.event.addDomListener(window, 'load', initMap);
-34.868633, -111.761734
+  google.maps.event.addDomListener(window, 'load', initMap);
+}
